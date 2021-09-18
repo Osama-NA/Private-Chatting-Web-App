9760f8c72da29f84953738a6a6c1367c2ca96c45
Osama Hajj Ali <ossama.nae12@gmail.com>
2021-09-18 18:16:40 +0300

    Change added

--- utils/db-connection.js
+++ utils/db-connection.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
     connectionLimit: process.env.DATABASE_CONNECTION_LIMIT,
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASS,
+    password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE
 });
 
