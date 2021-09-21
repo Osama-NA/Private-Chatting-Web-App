8a68a277893c54d8fcf0a46aff49e9a88a6d6ec3
Osama Hajj Ali <ossama.nae12@gmail.com>
2021-09-21 19:36:21 +0300

    Routing changes

--- controllers/check-joining-expired-room.js
+++ controllers/check-joining-expired-room.js
@@ -1,24 +1,22 @@
+const room = require("../utils/room");
 const localStorage = require("localStorage");
-let reload = true;
 
 //before redirecting to chat room, check if user is joining a expired room
 //value is set in localStorage in /controllers/check-room-data
 exports.joiningExpiredRoom = (req, res, next) => {
 
-    //reloading page once on every redirect to re check if room expired
-    //Needed since, the value to check expired room is set in a asynchronous 
-    //sql query callback, so the value wouldn't show up on the first redirect
-    if (reload) {
-        reload = false;
+    //reloading page once if it's not room creator being redirected chat room
+    //to re check if room expired. Needed since, the value to check expired 
+    //room is set in a asynchronous sql query callback, so the value wouldn't 
+    //show up on the first redirect
+    if (!room.getRoom()["access-one"] === 'true') {
         return res.redirect(req._parsedOriginalUrl.path); //redirecting to current path
     }
 
     if (localStorage.getItem("joining expired room") === 'true') {
-        reload = true; //resetting reload
         localStorage.removeItem("joining expired room", false);
         return res.redirect("/room-expired");
     }
 
-    reload = true; //resetting reload
     return next();
 }
