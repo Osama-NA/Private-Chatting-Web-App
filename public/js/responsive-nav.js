466f5e05659d1cb3daefbf46ba459d071f546e67
Osama Hajj Ali <ossama.nae12@gmail.com>
2021-10-21 03:25:33 +0300

    logo changes

--- public/js/responsive-nav.js
+++ public/js/responsive-nav.js
@@ -1,16 +1,16 @@
 const burger= document.querySelector('.burger');
 const menu= document.querySelector('.nav');
 
-burger.onclick= displayMenu;
+burger.onclick = displayMenu;
 
 //Function Which Hides And Displays Navigation Menu
 function displayMenu(){
     const style = getComputedStyle(menu); 
     const getDisplayStyle= style.display;
-    if(getDisplayStyle === "block"){
+    if(getDisplayStyle === "flex"){
         menu.style.display= "none";
     }
     else{
-        menu.style.display= "block";
+        menu.style.display= "flex";
     }
 }
